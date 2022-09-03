1660a5867a33efce7ea7e6373a65a070c515cae5
Gabriel Valle <gabrielvallerodrigues@gmail.com>
2022-09-03 12:00:43 -0300

    projeto InfoMovie finalizado

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,37 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import {BiCameraMovie, BiSearchAlt2} from 'react-icons/bi';
 
+import styles from './Navbar.module.css'
+
 const Navbar = () =>{
+    const [search, setSearch] = useState("")
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      console.log(search)
+
+      if(!search) return;
+
+      navigate(`/search?q=${search}`)
+      setSearch("");
+    }
+
     return(
-        <nav id="navbar">
+        <nav className={styles.navbar} >
         <h2>
           <Link to="/">
             <BiCameraMovie/> InfoMovies
           </Link>
         </h2>
-        <form>
-            <input type="text" placeholder="Busque um filme"/>
+        <form onSubmit={handleSubmit}>
+            <input
+             type="text"
+             placeholder="Busque um filme"
+             onChange={(e) => setSearch(e.target.value)}
+             value={search}
+             />
             <button type="submit">
                 <BiSearchAlt2/>
             </button>
